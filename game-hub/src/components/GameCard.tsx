db9caac5039f0787e75efe55509917f864ff8515
Vehiz <113064866+Vehiz@users.noreply.github.com>
2023-04-25 21:29:08 +0100

    Get optimized images

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Games } from '../hooks/useGames'
 import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
+import getCroppedImageUrl from '../services/image-url'
 
 interface Props {
     game: Games
@@ -11,11 +12,11 @@ interface Props {
 const GameCard = ({game}:Props) => {
   return (
     <Card borderRadius={10} overflow={'hidden'}>
-        <Image src={game.background_image}/>
+        <Image src={getCroppedImageUrl(game.background_image)}/>
         <CardBody>
             <Heading fontSize={'2xl'}>{game.name}</Heading>
             <HStack justifyContent={'space-between'}>
-            <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+            <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)}/>
             <CriticScore score={game.metacritic}/>
             </HStack>
         </CardBody>
--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -7,7 +7,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{xl: 5}} spacing={10} padding={10}>
+      <SimpleGrid columns={{sm:1, md:2, xl: 5}} spacing={10} padding={10}>
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
