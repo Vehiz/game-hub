b484e6a4b4c38fd9d3ff6a3012458d0acc534757
Vehiz <113064866+Vehiz@users.noreply.github.com>
2023-05-02 02:06:34 +0100

    display genres

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -10,12 +10,16 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, xl: 5 }} spacing={10} padding={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, xl: 5 }} spacing={3} padding={10}>
         {isLoading &&
-          skeletons.map((skeleton) => <GameCardContainer key={skeleton}><GameCardSkeleton /></GameCardContainer>)}
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
         {data.map((game) => (
           <GameCardContainer key={game.id}>
-              <GameCard  game={game} />
+            <GameCard game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
