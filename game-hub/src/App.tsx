1ff5d14e4b4c0f1cc02dcac437003cdc6592fb3b
Vehiz <113064866+Vehiz@users.noreply.github.com>
2023-05-03 03:06:34 +0100

    Filter games by platform

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -4,10 +4,12 @@ import GameGrid from './components/GameGrid'
 import GenreList from './components/GenreList'
 import NavBar from './components/NavBar'
 import PlatformSelector from './components/PlatformSelector'
+import { Platform } from './hooks/useGames'
 import { Genre } from './hooks/useGenre'
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
 
   return <Grid templateAreas={{
     base: `"nav" "main"`,
@@ -26,8 +28,8 @@ function App() {
       </GridItem>
     </Show>
     <GridItem area= "main">
-      <PlatformSelector/>
-      <GameGrid selectedGenre={selectedGenre}/>
+      <PlatformSelector selectedPlatform={selectedPlatform} onSelectedPlatform={(platform)=> setSelectedPlatform(platform)}/>
+      <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
     </GridItem>
   </Grid>    
 }
--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -5,24 +5,28 @@ import apiClient from "../services/api-client";
 import useData from "./useData";
 import { Genre } from "./useGenre";
 
-export interface Platform{
-    id: number
-    name: string
-    slug: string
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Games {
-    id: number;
-    name: string;
-    background_image: string
-    parent_platforms:{platform:Platform}[]
-    metacritic: number
-  }
-  
-  
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
 
-const useGames = (selectedGenre: Genre | null)=> useData<Games>('/games', {params: {genres: selectedGenre?.id}}, [selectedGenre?.id])
-   
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Games>(
+    "/games",
+    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
-
-export default useGames
+export default useGames;
