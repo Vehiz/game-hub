a64925889b3d8756d4733711b9b2780c7c7b6623
Vehiz <113064866+Vehiz@users.noreply.github.com>
2023-05-03 11:07:30 +0100

    Build sort selector

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,42 +1,60 @@
-import { Grid, GridItem, Show } from '@chakra-ui/react'
-import { useState } from 'react'
-import GameGrid from './components/GameGrid'
-import GenreList from './components/GenreList'
-import NavBar from './components/NavBar'
-import PlatformSelector from './components/PlatformSelector'
-import { Platform } from './hooks/useGames'
-import { Genre } from './hooks/useGenre'
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { useState } from "react";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+import NavBar from "./components/NavBar";
+import PlatformSelector from "./components/PlatformSelector";
+import SortSelector from "./components/SortSelector";
+import { Platform } from "./hooks/useGames";
+import { Genre } from "./hooks/useGenre";
 
-export interface GameQuery{
-  genre: Genre | null
-  platform: Platform | null
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
 }
 function App() {
-  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
-  return <Grid templateAreas={{
-    base: `"nav" "main"`,
-    lg: `"nav nav" "aside main"`
-  }}
-  templateColumns={{
-    base: '1fr',
-    lg: '200px 1fr'
-  }}>
-    <GridItem area= "nav">
-      <NavBar/>
-    </GridItem>
-    <Show above='lg'>
-      <GridItem paddingX='5px' area= "aside">
-        <GenreList selectedGenre={gameQuery.genre} onSelectedGenre={(genre)=> setGameQuery({...gameQuery, genre})}/>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
-    </Show>
-    <GridItem area= "main">
-      <PlatformSelector selectedPlatform={gameQuery.platform} onSelectedPlatform={(platform)=> setGameQuery({...gameQuery, platform})}/>
-      <GameGrid gameQuery={gameQuery}/>
-    </GridItem>
-  </Grid>    
+      <Show above="lg">
+        <GridItem paddingX="5px" area="aside">
+          <GenreList
+            selectedGenre={gameQuery.genre}
+            onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+          />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <HStack spacing={5} paddingLeft={9}>
+          <SortSelector />
+          <PlatformSelector
+            selectedPlatform={gameQuery.platform}
+            onSelectedPlatform={(platform) =>
+              setGameQuery({ ...gameQuery, platform })
+            }
+          />
+        </HStack>
+        <GameGrid gameQuery={gameQuery} />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
